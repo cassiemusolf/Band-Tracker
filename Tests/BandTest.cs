afd157fc112cbff6a08802d0efce3722c9eb2e7a
Cassie Musolf <cassiemusolf@gmail.com>
2017-03-03 09:30:06 -0800

    add save method for band object, pass test to see if it saved.

--- Tests/BandTest.cs
+++ Tests/BandTest.cs
@@ -34,6 +34,21 @@ namespace BandTracker
             Assert.Equal(firstBand, secondBand);
         }
 
+        [Fact]
+        public void Test_Save_SavesToDatabase()
+        {
+            //Arrange
+            Band testBand = new Band("John Mayer");
+
+            //Act
+            testBand.Save();
+            List<Band> result = Band.GetAll();
+            List<Band> testList = new List<Band>{testBand};
+
+            //Assert
+            Assert.Equal(testList, result);
+        }
+
         public void Dispose()
         {
           Band.DeleteAll();
