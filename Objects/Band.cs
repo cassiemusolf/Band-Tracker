0a68f759a096efadf3ab7e5669dc4a7bbc5c9f2d
Cassie Musolf <cassiemusolf@gmail.com>
2017-03-03 08:59:20 -0800

    add band and venue object files along with the test files. Add test to test if band database is empty at first. Add GetAll method.

---
+++ Objects/Band.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker
+{
+  public class Band
+  {
+    private int _id;
+    private string _name;
+
+    public Band(string Name, int Id = 0)
+    {
+        _id = Id;
+        _name = Name;
+    }
+
+    public int GetId()
+    {
+        return _id;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetName(string newName)
+    {
+        _name = newName;
+    }
+
+    public static List<Band> GetAll()
+    {
+        List<Band> All Bands = new List<Band>{}
+
+        SqlConnection conn = DB.Connection();
+        conn.Open();
+
+        SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
+        SqlDataReader rdr = cmd.ExecuteReader();
+
+        while(rdr.Read())
+        {
+            int bandId = rdr.GetInt32(0);
+            string bandName = rdr.GetString(1);
+            Band newBand = new Band(bandName, bandId);
+            AllBands.Add(newBand);
+        }
+
+        if (rdr != null)
+        {
+            rdr.Close();
+        }
+        if (conn != null)
+        {
+            conn.Close();
+        }
+        return AllBands;
+    }
---
+++ Tests/BandTest.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+    public class BandTest : IDisposable
+    {
+        public BandTest()
+        {
+            DBConfiguration.connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+        }
+
+        [Fact]
+        public void Test_EmptyAtFirst()
+        {
+            //Arrange, Act
+            int result = Band.GetAll().Count;
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        public void Dispose()
+        {
+          Band.DeleteAll();
+        }
+    }
+}
