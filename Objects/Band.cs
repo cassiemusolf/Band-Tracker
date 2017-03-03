d5938e511ee15ee76122e20b3b8eaee41e9cbb92
Cassie Musolf <cassiemusolf@gmail.com>
2017-03-03 09:02:19 -0800

    add delete all method for band object. Prepare to run first test.

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -4,57 +4,68 @@ using System;
 
 namespace BandTracker
 {
-  public class Band
-  {
-    private int _id;
-    private string _name;
-
-    public Band(string Name, int Id = 0)
+    public class Band
     {
-        _id = Id;
-        _name = Name;
-    }
+        private int _id;
+        private string _name;
 
-    public int GetId()
-    {
-        return _id;
-    }
-
-    public string GetName()
-    {
-        return _name;
-    }
-
-    public void SetName(string newName)
-    {
-        _name = newName;
-    }
-
-    public static List<Band> GetAll()
-    {
-        List<Band> All Bands = new List<Band>{}
-
-        SqlConnection conn = DB.Connection();
-        conn.Open();
-
-        SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
-        SqlDataReader rdr = cmd.ExecuteReader();
-
-        while(rdr.Read())
+        public Band(string Name, int Id = 0)
         {
-            int bandId = rdr.GetInt32(0);
-            string bandName = rdr.GetString(1);
-            Band newBand = new Band(bandName, bandId);
-            AllBands.Add(newBand);
+            _id = Id;
+            _name = Name;
         }
 
-        if (rdr != null)
+        public int GetId()
         {
-            rdr.Close();
+            return _id;
         }
-        if (conn != null)
+
+        public string GetName()
         {
+            return _name;
+        }
+
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+
+        public static List<Band> GetAll()
+        {
+            List<Band> All Bands = new List<Band>{}
+
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                int bandId = rdr.GetInt32(0);
+                string bandName = rdr.GetString(1);
+                Band newBand = new Band(bandName, bandId);
+                AllBands.Add(newBand);
+            }
+
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+            return AllBands;
+        }
+
+        public static void DeleteAll()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM bands;", conn);
+            cmd.ExecuteNonQuery();
             conn.Close();
         }
-        return AllBands;
     }
+}
