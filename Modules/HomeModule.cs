b10d9bc6189b4bc0e39809da2542983d568ecc28
Cassie Musolf <cassiemusolf@gmail.com>
2017-03-03 11:53:33 -0800

    add pages for both the band form and the venue form, route those using the home module.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -19,6 +19,28 @@ namespace BandTracker
               List<Venue> AllVenues = Venue.GetAll();
               return View["venues.cshtml", AllVenues];
             };
+            Get["/bands/new"] = _ => {
+                List<Venue> AllVenues = Venue.GetAll();
+                return View["bands_form.cshtml", AllVenues];
+            };
+            Post["/bands"] = _ => {
+                Band newBand = new Band(Request.Form["band-name"]);
+                newBand.Save();
+                newBand.AddVenue(Venue.Find(Request.Form["venue-id"]));
+                List<Band> AllBands = Band.GetAll();
+                return View["bands.cshtml", AllBands];
+            };
+            Get["/venues/new"] = _ => {
+                List<Band> AllBands = Band.GetAll();
+                return View["venues_form.cshtml", AllBands];
+            };
+            Post["/venues"] = _ => {
+                Venue newVenue = new Venue(Request.Form["venue-name"]);
+                newVenue.Save();
+                newVenue.AddBand(Band.Find(Request.Form["band-id"]));
+                List<Venue> AllVenues = Venue.GetAll();
+                return View["venues.cshtml", AllVenues];
+            };
         }
     }
 }
